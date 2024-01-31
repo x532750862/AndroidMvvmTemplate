6e41231518b459733ce8c209550ae62737e31ba9
GitHub Action <action@github.com>
2024-01-31 07:21:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/x532750862/androidmvvmtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/x532750862/androidmvvmtemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.x532750862.androidmvvmtemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
